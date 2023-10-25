471abb8f15136714f03697d6f76961890bc99fbd
Rysth <johnpalacios.t@gmail.com>
2023-10-25 12:32:59 -0500

    Create method update_counter in Post.

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,10 @@ class Post < ApplicationRecord
   belongs_to :user, class_name: 'User', foreign_key: 'author_id'
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
+
+  def update_counter
+    user.update(posts_counter: user.posts.count)
+  end
 end
 
 =begin 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_10_24_205021) do
+ActiveRecord::Schema[7.1].define(version: 2023_10_25_171048) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -49,7 +49,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_10_24_205021) do
     t.string "photo"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "post_counter"
+    t.integer "posts_counter"
     t.string "bio"
   end
 
