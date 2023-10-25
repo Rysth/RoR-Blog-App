62e311c313ee369f9736bbc712cac1c0f8986a40
Rysth <johnpalacios.t@gmail.com>
2023-10-25 13:22:37 -0500

    Fix Linter errors.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -6,27 +6,3 @@ class Comment < ApplicationRecord
     post.update(comments_counter: post.comments.count)
   end
 end
-
-#
-#
-#   CREATE | Queries to create Comments
-#
-#     Comment.create(post: first_post, user: second_user, text: 'Hi Tom!' )
-#     Comment.create(post: first_post, user: second_user, text: 'Hi John!' )
-#     Comment.create(post: first_post, user: second_user, text: 'Hi John 2!' )
-#     Comment.create(post: first_post, user: second_user, text: 'Hi Palacios!' )
-#     Comment.create(post: first_post, user: second_user, text: 'Hi Rysth!' )
-#     Comment.create(post: first_post, user: second_user, text: 'Hi Microverse!' )
-#
-#   READ | Queries to get all the Comments from a specific Post.
-#
-#     Comment.where(post_id: first_post.id)
-#
-#   UPDATE | Queries to update a Comment from a specific User.
-#
-#     Comment.find_by(user_id: second_user.id).update(text: 'Hello World!')
-#
-#   DELETE | Queries to delete all the Comments from a specific User and Post.
-#
-#     Comment.where(user_id: second_user.id, post_id: first_post.id).destroy_all
-#
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,28 +7,3 @@ class User < ApplicationRecord
     posts.order(created_at: :desc).limit(3)
   end
 end
-
-#
-#
-#   CREATE | Queries to create a User
-#
-#     first_user = User.create(name: 'Tom', 
-#      photo: 'https://unsplash.com/photos/F_-0BxGuVvo', 
-#      bio: 'Teacher from Mexico.')
-
-#     second_user = User.create(name: 'Lilly', 
-#     photo: 'https://unsplash.com/photos/F_-0BxGuVvo', 
-#     bio: 'Teacher from Poland.')
-#
-#   READ | Queries to count all the User
-#
-#     User.count()
-#
-#   UPDATE | Queries to update all the User
-#
-#     User.all.update_all(name: 'John')
-#
-#   DELETE | Queries to delete a User by his name and from a specific id.
-#
-#     User.where(name: 'John').destroy_all
-#
