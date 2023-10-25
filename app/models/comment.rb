f83f43fa560a5aedffcca439d63d770be4af0d1e
Rysth <johnpalacios.t@gmail.com>
2023-10-25 11:07:55 -0500

    Create Comments queries to handle creation.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,3 +2,14 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
 end
+
+=begin 
+  Queries to create Comments
+
+  Comment.create(post_id: first_post.id, user_id: second_user.id, text: 'Hi Tom!' )
+  Comment.create(post_id: first_post.id, user_id: second_user.id, text: 'Hi John!' )
+  Comment.create(post_id: first_post.id, user_id: second_user.id, text: 'Hi John 2!' )
+  Comment.create(post_id: first_post.id, user_id: second_user.id, text: 'Hi Palacios!' )
+  Comment.create(post_id: first_post.id, user_id: second_user.id, text: 'Hi Rysth!' )
+  Comment.create(post_id: first_post.id, user_id: second_user.id, text: 'Hi Microverse!' )
+=end
