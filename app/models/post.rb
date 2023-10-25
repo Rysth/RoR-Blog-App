de7dcabae58fb0a5ac7ed72ebff3f5ac58a6eb9c
Rysth <johnpalacios.t@gmail.com>
2023-10-25 11:41:45 -0500

    Create whole CRUD for Post.

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,10 +5,24 @@ class Post < ApplicationRecord
 end
 
 =begin 
-  Queries to create Post
+  
+  CREATE | Queries to create Post
 
-  first_post = Post.create(author_id: first_user.id, title: 'Hello', text: 'This is my first post')
-  second_post = Post.create(author_id: first_user.id, title: 'olleH', text: 'This is my second post')
-  third_post = Post.create(author_id: first_user.id, title: 'World', text: 'This is my third post')
-  fourth_post = Post.create(author_id: first_user.id, title: 'Games', text: 'This is my fourth post')
+    first_post = Post.create(author_id: first_user.id, title: 'Hello', text: 'This is my first post')
+    second_post = Post.create(author_id: first_user.id, title: 'olleH', text: 'This is my second post')
+    third_post = Post.create(author_id: first_user.id, title: 'World', text: 'This is my third post')
+    fourth_post = Post.create(author_id: first_user.id, title: 'Games', text: 'This is my fourth post')
+
+  READ | Queries to get all the Post
+
+    Post.all
+
+  UPDATE | Queries to update a Post from a specific id.
+
+    Post.find(first_post.id)
+
+  DELETE | Queries to delete a Post from a specific id.
+
+    Post.find(first_post.id).destroy
+
 =end
