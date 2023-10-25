2e8eee9146090b593bd0bd571460bb48f5f7ced6
Rysth <johnpalacios.t@gmail.com>
2023-10-25 11:02:36 -0500

    Create Post queries to handle creation.

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,3 +3,12 @@ class Post < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
 end
+
+=begin 
+  Queries to create Post
+
+  first_post = Post.create(author_id: first_user.id, title: 'Hello', text: 'This is my first post')
+  second_post = Post.create(author_id: first_user.id, title: 'olleH', text: 'This is my second post')
+  third_post = Post.create(author_id: first_user.id, title: 'World', text: 'This is my third post')
+  fourth_post = Post.create(author_id: first_user.id, title: 'Games', text: 'This is my fourth post')
+=end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   has_many :likes, dependent: :destroy
 end
 
-# Queries to create a User
-first_user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
-second_user = User.create(name: 'Lilly', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Poland.')
+=begin  
+  Queries to create a User
+  first_user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
+  second_user = User.create(name: 'Lilly', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Poland.')
+=end
