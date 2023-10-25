21d281fcdc8d4403912bfd050d7436af8ef67b5e
Rysth <johnpalacios.t@gmail.com>
2023-10-25 10:43:13 -0500

    Enhance has_many relationship in User, Post.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,3 +3,7 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
 end
+
+# Queries to create a User
+first_user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
+second_user = User.create(name: 'Lilly', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Poland.')
