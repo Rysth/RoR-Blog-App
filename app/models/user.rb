68af6105e48f88259ad1ca87fe9039a6f349d754
Rysth <johnpalacios.t@gmail.com>
2023-10-26 10:05:16 -0500

    Create method to check if posts_counter is not negative in User.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,20 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
 
+  # Validation: Name must not be blank.
   validates :name, presence: true
+  # Validation: PostsCounter must be an integer greater than or equal to zero.
+  validate :posts_counter_greater_than_zero, 
 
   def recent_post
     posts.order(created_at: :desc).limit(3)
+  end
+
+  private
+
+  def posts_counter_greater_than_zero
+    if :posts_counter < 0
+      errors.add(:posts_counter, 'PostsCounter must be an integer greater than or equal to zero.')
+    end
   end
 end
