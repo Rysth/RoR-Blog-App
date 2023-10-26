4f30a801129b75fc46c1fdaaadddaab69d4eefce
Rysth <johnpalacios.t@gmail.com>
2023-10-26 10:13:00 -0500

    Add title validation up to 250 characters in Posts.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,20 +1,21 @@
-class Post < ApplicationRecord
-  belongs_to :user, class_name: 'User', foreign_key: 'author_id'
-  has_many :comments, dependent: :destroy
-  has_many :likes, dependent: :destroy
+  class Post < ApplicationRecord
+    belongs_to :user, class_name: 'User', foreign_key: 'author_id'
+    has_many :comments, dependent: :destroy
+    has_many :likes, dependent: :destroy
 
-  after_save :update_counter
+    after_save :update_counter
 
-  # Validation: Title must not be blank.
-  validates :title, presence: true
+    # Validation: Title must not be blank & Title must not exceed 250 characters.
+    validates :title, presence: true, length: { maximum: 250 }
 
-  def recent_comments
-    comments.order(created_at: :desc).limit(5)
+
+    def recent_comments
+      comments.order(created_at: :desc).limit(5)
+    end
+
+    private
+
+    def update_counter
+      user.update(posts_counter: user.posts.count)
+    end
   end
-
-  private
-
-  def update_counter
-    user.update(posts_counter: user.posts.count)
-  end
-end
