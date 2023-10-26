1bb778de917f5ceb6773c4f014e47f7598af7cbd
Rysth <johnpalacios.t@gmail.com>
2023-10-26 13:11:38 -0500

    Create comments rspec test.

---
+++ spec/models/comment_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe Comment, type: :model do
+  fixtures :comments, :users, :posts
+
+  it "updates comments_counter after a new comment is created" do
+    comment = Comment.create(user: users(:one), post: posts(:one), text: "New comment")
+    original_comments_counter = posts(:one).comments_counter
+
+    expect(posts(:one).reload.comments_counter).to eq(original_comments_counter)
+  end
+end
