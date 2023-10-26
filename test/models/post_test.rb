9ae11928c7512d2871fa523828ca08777b3cc9a5
Rysth <johnpalacios.t@gmail.com>
2023-10-26 12:50:44 -0500

    Create recent_comments test for Post.

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,6 +1,7 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
+  fixtures :posts, :comments
 
   test "title should not be empty" do
     post = posts(:one)
@@ -30,6 +31,21 @@ class PostTest < ActiveSupport::TestCase
   test "likes_counter should not be greater than or equal to 0" do
     post = posts(:two)
     assert_not post.likes_counter >= 0
+  end
+
+  test "recent_comments returns five most recent comments" do
+    post = posts(:one)
+
+    recent_comments= post.recent_comments
+
+    puts recent_comments.join(' ')
+
+    assert_equal 5, recent_comments.length
+    assert_equal comments(:one), recent_comments[0]
+    assert_equal comments(:two), recent_comments[1]
+    assert_equal comments(:three), recent_comments[2]
+    assert_equal comments(:four), recent_comments[3]
+    assert_equal comments(:five), recent_comments[4]
   end
 
 end
