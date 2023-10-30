2612e3355f868b5a157d67b9389256548859ff83
Rysth <johnpalacios.t@gmail.com>
2023-10-30 13:41:45 -0500

    Fix rubocop linter errors.

--- spec/requests/posts_controller_spec.rb
+++ spec/requests/posts_controller_spec.rb
@@ -1,34 +1,33 @@
 require 'rails_helper'
 
-RSpec.describe "PostsControllers", type: :request do
-  describe "GET /users/:user_id/posts" do
-    it "return correct status" do
-      get "/users/1/posts"
+RSpec.describe 'PostsControllers', type: :request do
+  describe 'GET /users/:user_id/posts' do
+    it 'return correct status' do
+      get '/users/1/posts'
       expect(response.status).to eq(200)
     end
-    it "return the body correctly" do
-      get "/users/1/posts"
+    it 'return the body correctly' do
+      get '/users/1/posts'
       expect(response.body).not_to be_nil
     end
-    it "contains the correct placeholder text" do
-      get "/users/1/posts"
-      expect(response.body).to include("Here is the list of Posts.")
+    it 'contains the correct placeholder text' do
+      get '/users/1/posts'
+      expect(response.body).to include('Here is the list of Posts.')
     end
   end
 
-  describe "GET /users/:user_id/posts/:id" do
-    it "return correct status" do
-      get "/users/1/posts/1"
+  describe 'GET /users/:user_id/posts/:id' do
+    it 'return correct status' do
+      get '/users/1/posts/1'
       expect(response.status).to eq(200)
     end
-    it "return the body correctly" do
-      get "/users/1/posts/1"
+    it 'return the body correctly' do
+      get '/users/1/posts/1'
       expect(response.body).not_to be_nil
     end
-    it "contains the correct placeholder text" do
-      get "/users/1/posts/1"
-      expect(response.body).to include("Here is the Post information from an specific User.")
+    it 'contains the correct placeholder text' do
+      get '/users/1/posts/1'
+      expect(response.body).to include('Here is the Post information from an specific User.')
     end
   end
-
 end
--- spec/requests/users_controller_spec.rb
+++ spec/requests/users_controller_spec.rb
@@ -1,34 +1,33 @@
 require 'rails_helper'
 
-RSpec.describe "UsersControllers", type: :request do
-  describe "GET /users" do
-    it "return correct status" do
-      get "/users"
+RSpec.describe 'UsersControllers', type: :request do
+  describe 'GET /users' do
+    it 'return correct status' do
+      get '/users'
       expect(response.status).to eq(200)
     end
-    it "return the body correctly" do
-      get "/users"
+    it 'return the body correctly' do
+      get '/users'
       expect(response.body).not_to be_nil
     end
-    it "contains the correct placeholder text" do
-      get "/users"
-      expect(response.body).to include("Here is the list of Users.")
+    it 'contains the correct placeholder text' do
+      get '/users'
+      expect(response.body).to include('Here is the list of Users.')
     end
   end
 
-  describe "GET /users/:id" do
-    it "return correct status" do
-      get "/users/1"
+  describe 'GET /users/:id' do
+    it 'return correct status' do
+      get '/users/1'
       expect(response.status).to eq(200)
     end
-    it "return the body correctly" do
-      get "/users/1"
+    it 'return the body correctly' do
+      get '/users/1'
       expect(response.body).not_to be_nil
     end
-    it "contains the correct placeholder text" do
-      get "/users/1"
-      expect(response.body).to include("Here is the User information.")
+    it 'contains the correct placeholder text' do
+      get '/users/1'
+      expect(response.body).to include('Here is the User information.')
     end
   end
-  
 end
