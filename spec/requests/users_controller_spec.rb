bb2420c506b85f386a56bbc4479e52146ed1f8a8
Rysth <johnpalacios.t@gmail.com>
2023-10-30 13:30:55 -0500

    Create tests to check the Body and the Placeholder text.

--- spec/requests/users_controller_spec.rb
+++ spec/requests/users_controller_spec.rb
@@ -6,5 +6,13 @@ RSpec.describe "UsersControllers", type: :request do
       get "/users"
       expect(response.status).to eq(200)
     end
+    it "return the body correctly" do
+      get "/users"
+      expect(response.body).not_to be_nil
+    end
+    it "contains the correct placeholder text" do
+      get "/users"
+      expect(response.body).to include("Here is the list of Users.")
+    end
   end
 end
