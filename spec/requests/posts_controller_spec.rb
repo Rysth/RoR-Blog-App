3f8cb55fa399d11334b82e72a3b4bc58a89df6af
Rysth <johnpalacios.t@gmail.com>
2023-10-30 13:37:57 -0500

    Create tests for /users/:user_id/posts/:id on PostsController.

--- spec/requests/posts_controller_spec.rb
+++ spec/requests/posts_controller_spec.rb
@@ -16,4 +16,19 @@ RSpec.describe "PostsControllers", type: :request do
     end
   end
 
+  describe "GET /users/:user_id/posts/:id" do
+    it "return correct status" do
+      get "/users/1/posts/1"
+      expect(response.status).to eq(200)
+    end
+    it "return the body correctly" do
+      get "/users/1/posts/1"
+      expect(response.body).not_to be_nil
+    end
+    it "contains the correct placeholder text" do
+      get "/users/1/posts/1"
+      expect(response.body).to include("Here is the Post information from an specific User.")
+    end
+  end
+
 end
