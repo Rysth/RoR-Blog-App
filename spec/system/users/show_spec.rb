ca501a01b2693aeff8a1750bc346251bc6561247
Rysth <johnpalacios.t@gmail.com>
2023-11-07 15:02:38 -0500

    Add three comments to be displayed on show_spec from Users.

--- spec/system/users/show_spec.rb
+++ spec/system/users/show_spec.rb
@@ -4,7 +4,11 @@ RSpec.describe 'UsersShows', type: :system do
   before do
     driven_by(:rack_test)
     @user = User.create(name: 'Bravo', photo: 'test photo', bio: 'Specer', posts_counter: 0)
-    @post = Post.create(author_id: @user.id, title: 'Specing', text: 'Testing specs', comments_counter: 0,
+    @post_1 = Post.create(author_id: @user.id, title: 'Specing', text: 'Testing specs', comments_counter: 0,
+                        likes_counter: 0)
+    @post_2 = Post.create(author_id: @user.id, title: 'Hello', text: 'Testing specs', comments_counter: 0,
+                        likes_counter: 0)
+    @post_3 = Post.create(author_id: @user.id, title: 'World', text: 'Testing specs', comments_counter: 0,
                         likes_counter: 0)
     visit user_path(id: @user.id)
   end
@@ -18,7 +22,7 @@ RSpec.describe 'UsersShows', type: :system do
   end
 
   it 'Should render number of posts' do
-    expect(page).to have_content('Number of posts: 1')
+    expect(page).to have_content('Number of posts: 3')
   end
 
   it 'Should render user bio' do
@@ -26,7 +30,7 @@ RSpec.describe 'UsersShows', type: :system do
   end
 
   it 'Should render user first 3 posts' do
-    expect(page).to have_selector('#user-photo', count: 1)
+    expect(page).to have_selector('.post', count: 3)
   end
 
   it 'Should render see all posts button' do
@@ -35,7 +39,7 @@ RSpec.describe 'UsersShows', type: :system do
 
   it 'Should redirect to all post show page' do
     click_link('post-Specing')
-    expect(page).to have_current_path(user_post_path(user_id: @user.id, id: @post.id))
+    expect(page).to have_current_path(user_post_path(user_id: @user.id, id: @post_1.id))
   end
   it 'redirects to the user\'s posts page when the button See all posts is clicked' do
     click_link('See all posts')
