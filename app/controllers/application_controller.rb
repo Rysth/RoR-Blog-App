9983a1596d634776ca2cf05c2fc3fecb807870b9
Rysth <johnpalacios.t@gmail.com>
2023-11-10 10:02:11 -0500

    Update controllers and routes.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,19 +1,17 @@
-class ApplicationController < ActionController::API
+class ApplicationController < ActionController::Base
   # Must be disable for API applications.
-  # protect_from_forgery with: :exception
+  protect_from_forgery with: :exception
+  before_action :update_allowed_parameters, if: :devise_controller?
+  before_action :authenticate_user!
 
-  # before_action :update_allowed_parameters, if: :devise_controller?
-  # before_action :authenticate_user!
-# 
-  # protected
-# 
-  # def update_allowed_parameters
-  #   devise_parameter_sanitizer.permit(:sign_up) do |u|
-  #     u.permit(:name, :photo, :bio, :email, :password, :password_confirmation)
-  #   end
-  #   devise_parameter_sanitizer.permit(:account_update) do |u|
-  #     u.permit(:name, :photo, :bio, :email, :password, :current_password)
-  #   end
-  # end
-  
+  protected
+
+  def update_allowed_parameters
+    devise_parameter_sanitizer.permit(:sign_up) do |u|
+      u.permit(:name, :photo, :bio, :email, :password, :password_confirmation)
+    end
+    devise_parameter_sanitizer.permit(:account_update) do |u|
+      u.permit(:name, :photo, :bio, :email, :password, :current_password)
+    end
+  end
 end
